56bde5ecca816b53650fe4c9d0e31d6a2ac9da06
ForeverSc <fengshuochn@126.com>
2023-11-23 00:30:42 +0800

    fix: supported

--- packages/editor/src/page/EditorPage.tsx
+++ packages/editor/src/page/EditorPage.tsx
@@ -47,7 +47,7 @@ export default function EditorPage() {
           </Flex>
         </Flex>
       </Flex>
-      {supported && (
+      {!supported && (
         <Flex
           position={"fixed"}
           top={0}
