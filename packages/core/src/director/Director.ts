061885d1c8455680aad5acc355e4f0ef8f7ea93c
ForeverSc <fengshuochn@126.com>
2024-12-31 00:34:22 +0800

    refactor: enhance scene script execution flow and structure

--- packages/core/src/director/Director.ts
+++ packages/core/src/director/Director.ts
@@ -88,6 +88,11 @@ export interface SceneScript {
   config: SceneConfig;
 }
 
+interface SceneScriptExecutor {
+  frameCount: number;
+  install: () => () => void;
+}
+
 export interface Screenplay {
   name?: string;
   config?: unknown; // TODO: 待补充全局配置
@@ -175,14 +180,10 @@ export class Director {
     }
     const now = performance.now();
 
-    gsap.updateRoot(0); // 全局实例重置为0，否则重复执行时动画异常
-
     try {
-      const duration = this.parseScreenplay(screenplay);
+      const executors = this.parseScreenplay(screenplay);
 
-      this.registerUpdater();
-
-      return await this.run(duration);
+      return await this.run(executors);
     } finally {
       this.reset();
       log.info("action cost:", performance.now() - now);
@@ -217,59 +218,74 @@ export class Director {
     this.ticker.asyncHandlers = [];
   }
 
-  private parseScreenplay(screenplay: Screenplay): number {
-    let duration = 0;
+  private parseScreenplay(screenplay: Screenplay): SceneScriptExecutor[] {
     const { scenes: sceneScripts } = screenplay;
+    const sceneScriptExecutors: SceneScriptExecutor[] = [];
 
     for (const sceneScript of sceneScripts) {
-      duration = this.parseSceneScript(sceneScript, duration);
+      const executor = this.parseSceneScript(sceneScript);
+
+      sceneScriptExecutors.push(executor);
     }
 
-    return duration;
+    return sceneScriptExecutors;
   }
 
-  private parseSceneScript(
-    sceneScript: SceneScript,
-    prevSceneDuration: number,
-  ): number {
+  private parseSceneScript(sceneScript: SceneScript): SceneScriptExecutor {
     const { scene } = sceneScript;
     const { directives } = sceneScript;
-    let duration = prevSceneDuration;
+    let nextDirectiveStart = 0;
+    const directiveExecutors: Array<() => void> = [];
 
     // 初始化时，默认隐藏所有子元素
     scene.children.forEach((child) => {
       child.visible = false;
     });
 
-    // 注册场景切换
-    const currentSceneUpdater = () => {
-      const time = this.ticker.time;
+    const install = () => {
+      log.debug("switch scene exec:", this.ticker.time);
+      // 全局实例重置为0，否则重复执行时动画异常
+      gsap.updateRoot(0);
+      // 切换场景音频，清除非跨场景的音频
+      soundController.resetExceptUtilEnd();
+      // 切换场景
+      this.ticker.ctx.scene = scene;
+      // 注册指令执行器
+      directiveExecutors.forEach((executor) => {
+        this.ticker.add(executor);
+      });
 
-      if (
-        approximatelyEqual(
-          time,
-          prevSceneDuration,
-          1 / (this.rendererOptions.fps * 2),
-        )
-      ) {
-        // 切换场景音频，清除非跨场景的音频
-        soundController.resetExceptUtilEnd();
+      const frameUpdater = () => {
+        const time = this.ticker.time;
 
-        // 切换场景
-        this.ticker.ctx.scene = scene;
+        // 更新音频数据
+        soundController.update(time, this.rendererOptions.fps);
+        const updateAudioBuffers = async () => {
+          this.ticker.ctx.audioBuffers =
+            await soundController.getAudioBuffers();
+        };
+        this.ticker.asyncHandlers.push(updateAudioBuffers());
 
-        // 当前场景切换完成后，可以移除
-        this.ticker.remove(currentSceneUpdater);
+        // 动画ticker手动同步
+        gsap.updateRoot(time);
 
-        log.debug(
-          "update current scene:",
-          time,
-          prevSceneDuration,
-          time - prevSceneDuration,
-        );
-      }
+        // 渲染当前的场景
+        if (this.ticker.ctx.scene) {
+          this.renderer.render(this.ticker.ctx.scene);
+          this.ticker.ctx.imageSource = this.renderer.view as CanvasImageSource;
+        }
+      };
+
+      this.ticker.add(frameUpdater);
+
+      return () => {
+        this.ticker.remove(frameUpdater);
+        this.ticker.time = 0;
+        this.ticker.lastTime = -1;
+        this.ticker.ctx = {};
+        this.ticker.asyncHandlers = [];
+      };
     };
-    this.ticker.add(currentSceneUpdater);
 
     for (const item of directives) {
       const { directive: directiveName, params } = item;
@@ -281,7 +297,7 @@ export class Director {
       }
 
       // 指令执行时机
-      const executeTime = duration;
+      const executeTime = nextDirectiveStart;
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       const directive = new Directive(
@@ -302,86 +318,92 @@ export class Director {
           approximatelyEqual(
             time,
             directive.executeTime,
-            1 / (this.rendererOptions.fps * 2),
+            1 / this.rendererOptions.fps,
           )
         ) {
           log.debug(
             "execute directive:",
-            directive.constructor.name,
-            time,
-            directive.executeTime,
+            directive,
             time - directive.executeTime,
           );
           this.ticker.asyncHandlers.push(directive.execute());
-          // 指令执行完成后，可以移除
+          // 指令执行完成后移除
           this.ticker.remove(directiveExecutor);
         }
       };
-      this.ticker.add(directiveExecutor);
+      directiveExecutors.push(directiveExecutor);
 
-      duration += directive.getDuration();
+      nextDirectiveStart += directive.getDuration();
     }
 
-    return duration;
-  }
-
-  private registerUpdater() {
-    this.ticker.add(() => {
-      const time = this.ticker.time;
-
-      // 更新音频数据
-      soundController.update(time, this.rendererOptions.fps);
-      const updateAudioBuffers = async () => {
-        this.ticker.ctx.audioBuffers = await soundController.getAudioBuffers();
-      };
-      this.ticker.asyncHandlers.push(updateAudioBuffers());
-
-      // 动画ticker手动同步
-      gsap.updateRoot(time);
-
-      // 渲染当前的场景
-      if (this.ticker.ctx.scene) {
-        this.renderer.render(this.ticker.ctx.scene);
-        this.ticker.ctx.imageSource = this.renderer.view as CanvasImageSource;
-      }
-    });
-  }
-
-  private async run(duration: number) {
-    // 触发逐帧执行
     const { fps } = this.rendererOptions;
+    const duration = nextDirectiveStart;
     const frameCount = Math.ceil(duration * fps);
+
+    return {
+      frameCount,
+      install,
+    };
+  }
+
+  private async run(sceneScriptExecutors: SceneScriptExecutor[]) {
+    log.debug("sceneScriptExecutors", sceneScriptExecutors);
+
+    const { fps } = this.rendererOptions;
+    const frameCount = sceneScriptExecutors.reduce(
+      (acc, cur) => acc + cur.frameCount,
+      0,
+    );
+    const duration = frameCount / fps;
+    let frameIndex = 0;
 
     this.started = true;
 
-    for (let frameIndex = 0; frameIndex <= frameCount; frameIndex++) {
-      const frameTimeMS = (frameIndex / fps) * 1000;
+    // 按照场景顺序，触发逐帧执行
+    for (const sceneScriptExecutor of sceneScriptExecutors) {
+      // 执行切换场景, 并注册指令执行器
+      const uninstall = sceneScriptExecutor.install();
+      const sceneFrameCount = sceneScriptExecutor.frameCount;
 
-      if (this.started) {
-        this.ticker.time = frameTimeMS / 1000; // 拓展字段，记录当前tick的时间, 单位秒
-        this.ticker.update(frameTimeMS); // 手动触发ticker更新
+      for (
+        let sceneFrameIndex = 0;
+        sceneFrameIndex <= sceneFrameCount;
+        sceneFrameIndex++
+      ) {
+        const sceneFrameTimeMS = (sceneFrameIndex / fps) * 1000;
+        const frameTimeMS = (frameIndex / fps) * 1000;
 
-        // 等待所有异步任务完成
-        await Promise.all(this.ticker.asyncHandlers);
+        if (this.started) {
+          this.ticker.time = sceneFrameTimeMS / 1000; // 拓展字段，记录当前tick的时间, 单位秒
+          this.ticker.update(sceneFrameTimeMS); // 手动触发ticker更新
 
-        if (this.connecter?.connection) {
-          await this.connecter.handle({
-            timestamp: frameTimeMS,
-            imageSource: this.ticker.ctx.imageSource!,
-            audioBuffers: this.ticker.ctx.audioBuffers!,
-          });
+          // 等待所有异步任务完成
+          await Promise.all(this.ticker.asyncHandlers);
+
+          if (this.connecter?.connection) {
+            await this.connecter.handle({
+              timestamp: frameTimeMS, // 仅针对预览/合成时，需要给出全局时间
+              imageSource: this.ticker.ctx.imageSource!,
+              audioBuffers: this.ticker.ctx.audioBuffers!,
+            });
+          }
+
+          if (this.rendererOptions.onProgress) {
+            this.rendererOptions.onProgress(
+              (frameIndex / frameCount) * 100,
+              frameTimeMS / 1000,
+              duration,
+            );
+          }
+        } else {
+          break;
         }
 
-        if (this.rendererOptions.onProgress) {
-          this.rendererOptions.onProgress(
-            (frameIndex / frameCount) * 100,
-            frameTimeMS / 1000,
-            duration,
-          );
-        }
-      } else {
-        break;
+        frameIndex++;
       }
+
+      // 当前场景执行完成，卸载相关的监听器
+      uninstall();
     }
 
     let result;
